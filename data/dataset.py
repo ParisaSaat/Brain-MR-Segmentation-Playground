4c6034c2037c12a27dcc442647ba6552afea5a64
parisa <parisa.saat@gmail.com>
2021-09-15 16:34:05 -0600

    update dataset docs remove comment

--- data/dataset.py
+++ data/dataset.py
@@ -4,27 +4,15 @@ from medicaltorch.datasets import MRI2DSegmentationDataset
 
 
 class CC359(MRI2DSegmentationDataset):
-    """This is the ?? dataset.
+    """This is the CC359 dataset.
 
-    :param root_dir: the directory containing the training dataset.
-    :param site_ids: a list of site ids to filter (i.e. ['philips', 'ge']).
+    :param img_root_dir: the directory containing the MRI images.
+    :param gt_root_dir: the directory containing the ground truth masks.
     :param transform: the transformations that should be applied.
     :param cache: if the data should be cached in memory or not.
     :param slice_axis: axis to make the slicing (default axial).
-
-    .. note:: This dataset assumes that you only have one class in your
-              ground truth mask (w/ 0's and 1's). It also doesn't
-              automatically resample the dataset.
-
-    .. seealso::
-        Prados, F., et al (2017). Spinal cord grey matter
-        segmentation challenge. NeuroImage, 152, 312–329.
-        https://doi.org/10.1016/j.neuroimage.2017.03.010
-
-        Challenge Website:
-        http://cmictig.cs.ucl.ac.uk/spinal-cord-grey-matter-segmentation-challenge
     """
-    NUM_SITES = 6
+    NUM_SITES = 2
     NUM_SUBJECTS = 359
 
     def __init__(self, img_root_dir, slice_axis, gt_root_dir=None, file_ids=None, cache=True, transform=None,
@@ -43,7 +31,6 @@ class CC359(MRI2DSegmentationDataset):
             gt_filename = self._build_input_filename(file_id, True)
 
             img_filename = os.path.join(self.img_root_dir, img_filename)
-            # print('file_id:', file_id, 'img_filename:', img_filename)
             gt_filename = os.path.join(self.gt_root_dir, gt_filename)
 
             if not self.labeled:
