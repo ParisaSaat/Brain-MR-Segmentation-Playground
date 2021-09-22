36766083a088ccd7bdab14b0dadd1809981d941f
parisa <parisa.saat@gmail.com>
2021-09-22 14:44:11 -0600

    remove load_dataset, handling data patching, and add convert_array_to_dataset

--- data/utils.py
+++ data/utils.py
@@ -4,8 +4,10 @@ import medicaltorch.datasets as mt_datasets
 import medicaltorch.filters as mt_filters
 import medicaltorch.transforms as mt_transforms
 import numpy as np
+import torch
 import torchvision as tv
-from torch.utils.data import DataLoader
+from sklearn.feature_extraction.image import extract_patches_2d
+from torch.utils.data import TensorDataset
 
 from data.dataset import CC359
 
@@ -31,20 +33,21 @@ def get_dataset(img_root_dir, gt_root_dir, slice_axis):
     return dataset
 
 
-def load_dataset(dataset, batch_size, num_workers):
-    # data augmentation
-    transform = tv.transforms.Compose([
-        mt_transforms.CenterCrop2D(get_min_max_brain_interval),
-        mt_transforms.ElasticTransform(alpha_range=(28.0, 30.0),
-                                       sigma_range=(3.5, 4.0),
-                                       p=0.3),
-        mt_transforms.RandomAffine(degrees=4.6,
-                                   scale=(0.98, 1.02),
-                                   translate=(0.03, 0.03)),
-        mt_transforms.RandomTensorChannelShift((-0.10, 0.10)),
-        mt_transforms.ToTensor(),
-        mt_transforms.NormalizeInstance(),
-    ])
+def patch_data(dataset, patch_size, max_patches):
+    data_count = len(dataset)
+    total_patches_count = data_count * max_patches
+    images_patches = np.ndarray((total_patches_count, patch_size[0], patch_size[1]), dtype=np.float32)
+    masks_patches = np.ndarray((total_patches_count, patch_size[0], patch_size[1]), dtype=np.uint8)
+
+    random_value = np.random.randint(100)
+    for i in range(data_count):
+        data = dataset[i]
+        image = data.get('input')
+        mask = data.get('gt')
+        image_patches = extract_patches_2d(np.array(image), patch_size, max_patches, random_state=random_value)
+        mask_patches = extract_patches_2d(np.array(mask), patch_size, max_patches, random_state=random_value)
+        images_patches[i * max_patches:max_patches * (i + 1)] = image_patches
+        masks_patches[i * max_patches:max_patches * (i + 1)] = mask_patches
 
     dataset.set_transform(transform)
 
