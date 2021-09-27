18911f6f187b6b8b39924418db82566a9e069506
parisa <parisa.saat@gmail.com>
2021-09-26 20:15:50 -0600

    update save_patches

--- data/utils.py
+++ data/utils.py
@@ -1,4 +1,4 @@
-from os import listdir
+from os import makedirs
 
 import medicaltorch.filters as mt_filters
 import medicaltorch.transforms as mt_transforms
@@ -8,6 +8,7 @@ import torchvision as tv
 from sklearn.feature_extraction.image import extract_patches_2d
 from torch.utils.data import TensorDataset
 
+from config.io import NPY_ROOT
 from data.dataset import CC359
 
 
@@ -39,8 +40,8 @@ def patch_data(dataset, patch_size, max_patches):
     random_value = np.random.randint(100)
     for i in range(data_count):
         data = dataset[i]
-        image = data.get('input')
-        mask = data.get('gt')
+        image = data.get('input')[0]
+        mask = data.get('gt')[0]
         image_patches = extract_patches_2d(np.array(image), patch_size, max_patches, random_state=random_value)
         mask_patches = extract_patches_2d(np.array(mask), patch_size, max_patches, random_state=random_value)
         images_patches[i * max_patches:max_patches * (i + 1)] = image_patches
@@ -50,8 +51,9 @@ def patch_data(dataset, patch_size, max_patches):
 
 
 def save_patches(images_patches, masks_patches, images_patches_path, masks_patches_path):
-    np.save(images_patches_path + '.npy', images_patches.astype(np.float32))
-    np.save(masks_patches_path + '.npy', masks_patches.astype(np.uint8))
+    makedirs(NPY_ROOT)
+    np.save(images_patches_path, images_patches.astype(np.float32))
+    np.save(masks_patches_path, masks_patches.astype(np.uint8))
 
 
 def convert_array_to_dataset(x_arr, y_arr):
