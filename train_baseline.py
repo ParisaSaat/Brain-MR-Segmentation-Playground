a3b81cfd5e3f6173ad4493b5ce440489d8b8f86a
parisa <parisa.saat@gmail.com>
2021-09-27 01:09:01 -0600

    update parser

--- train_baseline.py
+++ train_baseline.py
@@ -13,11 +13,13 @@ def create_parser():
     parser.add_argument('-num_workers', type=int, default=16, help='number of workers')
     parser.add_argument('-num_epochs', type=int, default=1, help='number of epochs to train for')
     parser.add_argument('-experiment_name', type=str, default='', help='experiment name')
-    parser.add_argument('-lr', type=float, default=1e-3, help='learning rate of the optimizer')
+    parser.add_argument('-initial_lr', type=float, default=1e-3, help='learning rate of the optimizer')
+    parser.add_argument('-initial_lr_rampup', type=float, default=50, help='initial learning rate rampup')
     parser.add_argument('-decay', type=float, default=0.995, help='learning rate of the optimizer')
     parser.add_argument('-validation_split', type=float, default=0.1, help='validation split for training')
     parser.add_argument('-patience', type=int, default=10, help='early stopping patience')
-    parser.add_argument('-consistency_loss', type=int, default=3, help='consistency loss')
+    parser.add_argument('-consistency_loss', type=str, default='dice', help='consistency loss')
+    parser.add_argument('-drop_rate', type=int, default=0.5, help='model drop rate')
 
     opt = parser.parse_args()
     return opt
