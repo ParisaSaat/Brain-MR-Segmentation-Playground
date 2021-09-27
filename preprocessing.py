3115c803116cea153187a2b71434a83433b9bc6e
parisa <parisa.saat@gmail.com>
2021-09-26 20:39:10 -0600

    refactor parser

--- preprocessing.py
+++ preprocessing.py
@@ -11,27 +11,19 @@ from config.param import Plane, TEST_RATIO
 from data.utils import get_dataset, patch_data, save_patches
 
 
-def preprocess():
+def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-batch_size', type=int, default=16, help='input batch size')
-    parser.add_argument('-num_workers', type=int, default=16, help='number of workers')
-    parser.add_argument('-num_epochs', type=int, default=1, help='number of epochs to train for')
-    parser.add_argument('-experiment_name', type=str, default='', help='experiment name')
-    parser.add_argument('-lr', type=float, default=1e-3, help='learning rate of the optimizer')
-    parser.add_argument('-decay', type=float, default=0.995, help='learning rate of the optimizer')
-    parser.add_argument('-validation_split', type=float, default=0.1, help='validation split for training')
-    parser.add_argument('-patience', type=int, default=10, help='early stopping patience')
     parser.add_argument('-patch_size_row', type=int, default='128', help='patch width')
     parser.add_argument('-patch_size_col', type=int, default='128', help='patch height')
     parser.add_argument('-max_patches', type=int, default=3, help='number of patches per slice')
-    parser.add_argument('-consistency_loss', type=int, default=3, help='consistency loss')
-    parser.add_argument('-imgs_dir', type=str, default='imgs_train',
-                        help='images directory name')
-    parser.add_argument('-masks_dir', type=str, default='imgs_masks_train',
-                        help='mask directory name')
+    parser.add_argument('-imgs_dir', type=str, default='imgs_train', help='images directory name')
+    parser.add_argument('-masks_dir', type=str, default='imgs_masks_train', help='mask directory name')
 
     opt = parser.parse_args()
+    return opt
 
+
+def preprocess(opt):
     patch_size = opt.patch_size_row, opt.patch_size_col
 
     if torch.cuda.is_available():
@@ -76,4 +68,5 @@ def preprocess():
 
 
 if __name__ == '__main__':
-    preprocess()
+    options = create_parser()
+    preprocess(options)
