a1ba49a3ef6275aea285de0b42f4ca6aa4c228ee
parisasaat <parisa.saat@gmail.com>
2021-10-13 20:11:08 -0600

    Update postprocessing.py

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,13 +1,13 @@
 import argparse
+import shutil
+from os import listdir, makedirs
 
+from sklearn.model_selection import train_test_split
+
+from config.io import *
 from config.param import Plane, TEST_RATIO
 from data.dataset import CC359
-from data.utils import min_max_normalization, get_dataset
-import medicaltorch.filters as mt_filters
-from os import listdir, makedirs
-from sklearn.model_selection import train_test_split
-from config.io import *
-import shutil
+from data.utils import min_max_normalization
 
 
 def split_data(imgs_dir, masks_dir):
@@ -22,22 +22,18 @@ def split_data(imgs_dir, masks_dir):
     train_files, test_files = train_test_split(file_ids, test_size=TEST_RATIO, random_state=42, shuffle=True,
                                                stratify=vendors)
 
-    makedirs(SOURCE_TRAIN_IMAGES_PATH, exist_ok=True)
-    makedirs(SOURCE_TRAIN_MASKS_PATH, exist_ok=True)
-    makedirs(SOURCE_TEST_IMAGES_PATH, exist_ok=True)
-    makedirs(SOURCE_TEST_MASKS_PATH, exist_ok=True)
+    copy_files(train_files, imgs_dir, SOURCE_TRAIN_IMAGES_PATH, IMAGE_FILE_TEMPLATE)
+    copy_files(train_files, masks_dir, SOURCE_TRAIN_MASKS_PATH, MASK_FILE_TEMPLATE)
+    copy_files(test_files, imgs_dir, SOURCE_VAL_IMAGES_PATH, IMAGE_FILE_TEMPLATE)
+    copy_files(test_files, masks_dir, SOURCE_VAL_MASKS_PATH, MASK_FILE_TEMPLATE)
 
-    for file_id in train_files:
-        shutil.copy(path.join(imgs_dir, IMAGE_FILE_TEMPLATE.format(file_id=file_id)), SOURCE_TRAIN_IMAGES_PATH)
-        shutil.copy(path.join(masks_dir, MASK_FILE_TEMPLATE.format(file_id=file_id)), SOURCE_TRAIN_MASKS_PATH)
-    for file_id in test_files:
-        shutil.copy(path.join(imgs_dir, IMAGE_FILE_TEMPLATE.format(file_id=file_id)), SOURCE_TEST_IMAGES_PATH)
-        shutil.copy(path.join(masks_dir, MASK_FILE_TEMPLATE.format(file_id=file_id)), SOURCE_TEST_MASKS_PATH)
-    train_dataset = get_dataset(SOURCE_TRAIN_IMAGES_PATH, SOURCE_TRAIN_MASKS_PATH, train_files, tag)
-    val_dataset = get_dataset(SOURCE_VAL_IMAGES_PATH, SOURCE_VAL_MASKS_PATH, test_files, tag)
-    print('train dataset size:', len(train_dataset))
-    print('val dataset size:', len(val_dataset))
-    return train_dataset, val_dataset
+    return train_files, test_files
+
+
+def copy_files(file_ids, source, dest, name_template):
+    makedirs(dest, exist_ok=True)
+    for file_id in file_ids:
+        shutil.copy(path.join(source, name_template.format(file_id=file_id)), dest)
 
 
 def create_parser():
@@ -46,17 +42,20 @@ def create_parser():
     parser.add_argument('-masks_dir', type=str, default='imgs_masks_train', help='mask directory name')
     parser.add_argument('-num_epochs', type=int, default=5, help='number of epochs to train for')
     parser.add_argument('-normalize', type=bool, default=False, help='Do min max normalization on dataset')
+    parser.add_argument('-plane', type=int, default=Plane.SAGITTAL.value, help='2D plane')
 
     opt = parser.parse_args()
     return opt
 
 
 def preprocess(opt):
-    train_dataset, validation_dataset = split_data(opt.imgs_dir, opt.masks_dir)
-    train_dataset.save(SOURCE_TRAIN_IMAGES_PATH, SOURCE_TRAIN_MASKS_PATH)
-    validation_dataset.save(SOURCE_VAL_IMAGES_PATH, SOURCE_VAL_MASKS_PATH)
-
-
+    train_files, val_files = split_data(opt.imgs_dir, opt.masks_dir)
+    train_set = CC359(SOURCE_TRAIN_IMAGES_PATH, SOURCE_TRAIN_MASKS_PATH, opt.plane, train_files,
+                      normalizer=min_max_normalization)
+    validation_set = CC359(SOURCE_VAL_IMAGES_PATH, SOURCE_VAL_MASKS_PATH, opt.plane, val_files,
+                           normalizer=min_max_normalization)
+    train_set.save_slices(SOURCE_SLICES_TRAIN_IMAGES_PATH, SOURCE_SLICES_TRAIN_MASKS_PATH)
+    validation_set.save_slices(SOURCE_SLICES_VAL_IMAGES_PATH, SOURCE_SLICES_VAL_MASKS_PATH)
 
 
 if __name__ == '__main__':
