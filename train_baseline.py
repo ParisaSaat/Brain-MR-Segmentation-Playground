f5cb62ad919851ec1131b9d9ca679f88e763f497
parisasaat <parisa.saat@gmail.com>
2021-10-18 15:20:22 -0600

    finalize train

--- train_baseline.py
+++ train_baseline.py
@@ -3,31 +3,29 @@ import time
 from collections import defaultdict
 from os import listdir
 
+import albumentations as A
 import medicaltorch.losses as mt_losses
 import medicaltorch.metrics as mt_metrics
 import numpy as np
 import torch
-import torchvision.utils as vutils
+from albumentations.pytorch import ToTensorV2
 from tensorboardX import SummaryWriter
 from torch.utils.data import DataLoader
 from tqdm import *
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
 
 from config.io import *
 from config.param import LAMBDA
-from data.utils import convert_array_to_dataset
-from models.baseline import Unet
 from data.dataset import BrainMRI2D
+from models.baseline import Unet
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-batch_size', type=int, default=16, help='input batch size')
+    parser.add_argument('-batch_size', type=int, default=32, help='input batch size')
     parser.add_argument('-patch_height', type=int, default=128, help='patch size')
     parser.add_argument('-patch_width', type=int, default=128, help='patch size')
     parser.add_argument('-num_workers', type=int, default=16, help='number of workers')
-    parser.add_argument('-num_epochs', type=int, default=1, help='number of epochs to train for')
+    parser.add_argument('-num_epochs', type=int, default=30, help='number of epochs to train for')
     parser.add_argument('-experiment_name', type=str, default='', help='experiment name')
     parser.add_argument('-initial_lr', type=float, default=1e-3, help='learning rate of the optimizer')
     parser.add_argument('-initial_lr_rampup', type=float, default=50, help='initial learning rate rampup')
@@ -66,16 +64,7 @@ def get_current_consistency_weight(weight, epoch, rampup):
     return weight * sigmoid_rampup(epoch, rampup)
 
 
-def load_data_patches(img_path, msk_path, batch_size, num_workers):
-    images_patches = np.load(img_path)
-    masks_patches = np.load(msk_path)
-    dataset = convert_array_to_dataset(images_patches, masks_patches)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=num_workers,
-                            pin_memory=True)
-    return dataloader
-
-
-def validation(model, loader, writer, metric_fns, epoch, prefix):
+def validation(model, loader, writer, metric_fns, epoch):
     val_loss = 0.0
 
     num_samples = 0
@@ -84,7 +73,7 @@ def validation(model, loader, writer, metric_fns, epoch, prefix):
     result_dict = defaultdict(float)
 
     for i, batch in enumerate(loader):
-        image_data, mask_data = batch["input"], batch["gt"]
+        image_data, mask_data = batch['image'], batch['mask']
 
         image_data_gpu = image_data.cuda()
         mask_data_gpu = image_data.cuda()
@@ -97,16 +86,16 @@ def validation(model, loader, writer, metric_fns, epoch, prefix):
         masks = mask_data_gpu.cpu().numpy().astype(np.uint8)
         masks = masks.squeeze(axis=1)
 
-        preds = model_out.cpu().numpy()
-        preds = preds.squeeze(axis=1)
+        predictions = model_out.cpu().numpy()
+        predictions = predictions.squeeze(axis=1)
 
         for metric_fn in metric_fns:
-            for prediction, mask in zip(preds, masks):
+            for prediction, mask in zip(predictions, masks):
                 res = metric_fn(prediction, mask)
                 dict_key = 'val_{}'.format(metric_fn.__name__)
                 result_dict[dict_key] += res
 
-        num_samples += len(preds)
+        num_samples += len(predictions)
         num_steps += 1
 
     val_loss_avg = val_loss / num_steps
@@ -114,14 +103,13 @@ def validation(model, loader, writer, metric_fns, epoch, prefix):
     for key, val in result_dict.items():
         result_dict[key] = val / num_samples
 
-    writer.add_scalars(prefix + '_losses', {prefix + '_loss': val_loss_avg}, epoch)
-    writer.add_scalars(prefix + '_metrics', result_dict, epoch)
+    writer.add_scalars('losses', {'loss': val_loss_avg}, epoch)
+    writer.add_scalars('metrics', result_dict, epoch)
 
 
 def get_dataloader(image_dir, mask_dir, batch_size, transform):
     image_files = listdir(image_dir)
-    mask_files = listdir(mask_dir)
-    dataset = BrainMRI2D(image_files, mask_files, transform=transform)
+    dataset = BrainMRI2D(image_dir, mask_dir, file_ids=image_files, transform=transform)
     dataloader = DataLoader(dataset, batch_size)
     return dataloader
 
@@ -135,7 +123,6 @@ def train(opt):
 
     train_transform = A.Compose(
         [
-            A.Resize(),
             A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=10, p=0.5),
             A.RandomCrop(height=patch_size[0], width=patch_size[1]),
             A.RandomBrightnessContrast(p=0.5),
@@ -145,7 +132,7 @@ def train(opt):
 
     val_transform = A.Compose(
         [
-            A.CenterCrop(height=128, width=128),
+            A.CenterCrop(height=patch_size[0], width=patch_size[1]),
             ToTensorV2(),
         ]
     )
@@ -155,7 +142,7 @@ def train(opt):
     validation_dataloader = get_dataloader(SOURCE_SLICES_VAL_IMAGES_PATH, SOURCE_SLICES_VAL_IMAGES_PATH, opt.batch_size,
                                            val_transform)
 
-    model = Unet(drop_rate=opt.drop_rate, bn_momentum=opt.bn_momentum)
+    model = Unet(drop_rate=opt.drop_rate)
     model.cuda()
 
     optimizer = torch.optim.Adam(model.parameters(), weight_decay=LAMBDA, lr=opt.initial_lr)
@@ -184,58 +171,41 @@ def train(opt):
         loss_total = 0.0
 
         num_steps = 0
-    #
-    #     for i, train_batch in enumerate(train_dataloader):
-    #         train_image, train_mask = train_batch["input"], train_batch["gt"]
-    #         train_image = train_image.cuda()
-    #         train_mask = train_mask.cuda()
-    #         prediction = model(train_image)
-    #         loss = mt_losses.dice_loss(prediction, train_mask)
-    #         optimizer.zero_grad()
-    #         loss.backward()
-    #
-    #         optimizer.step()
-    #
-    #         loss_total += loss.item()
-    #
-    #         num_steps += 1
-    #
-    #     npy_prediction = prediction.detach().cpu().numpy()
-    #     writer.add_histogram("Prediction Hist", npy_prediction, epoch)
-    #
-    #     loss_avg = loss_total / num_steps
-    #
-    #     tqdm.write("Steps p/ Epoch: {}".format(num_steps))
-    #     tqdm.write("Class Loss: {:.6f}".format(loss_avg))
-    #
-    #     if opt.write_images and epoch % opt.write_images_interval == 0:
-    #         try:
-    #             plot_img = vutils.make_grid(prediction, normalize=True, scale_each=True)
-    #             writer.add_image('Train Source Prediction', plot_img, epoch)
-    #
-    #             plot_img = vutils.make_grid(train_image, normalize=True, scale_each=True)
-    #             writer.add_image('Train Source Input', plot_img, epoch)
-    #
-    #             plot_img = vutils.make_grid(train_mask, normalize=True, scale_each=True)
-    #             writer.add_image('Train Source Ground Truth', plot_img, epoch)
-    #         except:
-    #             tqdm.write("*** Error writing images ***")
-    #
-    #     writer.add_scalars('losses', {'loss': loss_avg}, epoch)
-    #
-    #     model.eval()
-    #
-    #     metric_fns = [mt_metrics.dice_score, mt_metrics.jaccard_score, mt_metrics.hausdorff_score,
-    #                   mt_metrics.precision_score, mt_metrics.recall_score,
-    #                   mt_metrics.specificity_score, mt_metrics.intersection_over_union,
-    #                   mt_metrics.accuracy_score]
-    #
-    #     validation(model, valid_dataloader, writer, metric_fns, epoch, opt)
-    #
-    #     end_time = time.time()
-    #     total_time = end_time - start_time
-    #     tqdm.write("Epoch {} took {:.2f} seconds.".format(epoch, total_time))
-    #
+
+        for i, train_batch in enumerate(train_dataloader):
+            train_image, train_mask = train_batch['image'], train_batch['mask']
+            train_image = train_image.cuda()
+            train_mask = train_mask.cuda()
+            prediction = model(train_image)
+            loss = mt_losses.dice_loss(prediction, train_mask)
+            optimizer.zero_grad()
+            loss.backward()
+
+            optimizer.step()
+
+            loss_total += loss.item()
+            #
+            num_steps += 1
+
+        loss_avg = loss_total / num_steps
+
+        tqdm.write("Steps p/ Epoch: {}".format(num_steps))
+        tqdm.write("Class Loss: {:.6f}".format(loss_avg))
+
+        writer.add_scalars('losses', {'loss': loss_avg}, epoch)
+
+        model.eval()
+
+        metric_fns = [mt_metrics.dice_score, mt_metrics.jaccard_score, mt_metrics.hausdorff_score,
+                      mt_metrics.precision_score, mt_metrics.recall_score,
+                      mt_metrics.specificity_score, mt_metrics.intersection_over_union,
+                      mt_metrics.accuracy_score]
+
+        validation(model, validation_dataloader, writer, metric_fns, epoch, opt)
+
+        end_time = time.time()
+        total_time = end_time - start_time
+        tqdm.write("Epoch {} took {:.2f} seconds.".format(epoch, total_time))
 
 
 if __name__ == '__main__':
