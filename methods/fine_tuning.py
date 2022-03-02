b638c3a3e8bee8ee2c31012826726cf76c876346
parisasaat <parisa.saat@gmail.com>
2022-03-02 15:24:08 -0700

    refactor fine-tuning

--- methods/fine_tuning.py
+++ methods/fine_tuning.py
@@ -1,4 +1,3 @@
-import argparse
 import time
 
 import albumentations as A
@@ -9,38 +8,23 @@ from tqdm import *
 
 from config.io import *
 from config.param import *
+from data.utils import get_dataloader
 from models.utils import dice_score
-from train_baseline import get_dataloader
-from train_baseline import scheduler
+from models.utils import scheduler
 
 
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-batch_size', type=int, default=32, help='input batch size')
-    parser.add_argument('-data_dir', type=str, help='images directory name')
-    parser.add_argument('-mode', type=str, help='fine-tuning mode')
-    parser.add_argument('-model_name', type=str, default='baseline', help='model to load for inference')
-    parser.add_argument('-experiment_name', type=str, default='tl', help='experiment name')
-    parser.add_argument('-num_epochs', type=int, default=15, help='number of epochs to train for')
-    parser.add_argument('-initial_lr', type=float, default=0.000125, help='learning rate of the optimizer')
-    parser.add_argument('-patch_height', type=int, default=128, help='patch size')
-    parser.add_argument('-patch_width', type=int, default=128, help='patch size')
-    parser.add_argument('-problem', type=str, default='skull-stripping', help='segmentation problem')
-    opt = parser.parse_args()
-    return opt
-
-
-def tl(opt):
-    num_epochs = opt.num_epochs
-    mode = opt.mode
-    img_pth = 'images_wgc' if opt.problem == 'wgc' else 'images'
-    msk_pth = 'masks_wgc' if opt.problem == 'wgc' else 'masks'
-    out_channels = 4 if opt.problem == 'wgc' else 1
+def cmd_train(opt):
+    num_epochs = opt["num_epochs"]
+    mode = opt["mode"]
+    problem = opt["problem"]
+    img_pth = 'images_wgc' if problem == 'wgc' else 'images'
+    msk_pth = 'masks_wgc' if problem == 'wgc' else 'masks'
+    out_channels = 4 if problem == 'wgc' else 1
     if torch.cuda.is_available():
         torch.cuda.set_device("cuda:0")
     start_time = time.time()
-    patch_size = (opt.patch_height, opt.patch_width)
-    data_dir = opt.data_dir
+    patch_size = (opt["patch_height"], opt["patch_width"])
+    data_dir = opt["data_dir"]
 
     train_transform = A.Compose(
         [
@@ -50,10 +34,10 @@ def tl(opt):
     )
 
     train_dataloader = get_dataloader(os.path.join(data_dir, img_pth), os.path.join(data_dir, msk_pth),
-                                      opt.batch_size, train_transform)
+                                      opt["batch_size"], train_transform)
     tl_size = len(train_dataloader) / 4
 
-    model = torch.load(MODEL_PATH.format(model_name=opt.model_name))
+    model = torch.load(MODEL_PATH.format(model_name=opt["model_name"]))
     model.cuda()
     ct = 0
     for child in model.children():
@@ -61,10 +45,10 @@ def tl(opt):
             for param in child.parameters():
                 param.requires_grad = False
         ct += 1
-    optimizer = torch.optim.Adam(model.parameters(), weight_decay=LAMBDA, lr=opt.initial_lr)
-    experiment_name = opt.experiment_name
+    optimizer = torch.optim.Adam(model.parameters(), weight_decay=LAMBDA, lr=opt["initial_lr"])
+    experiment_name = opt["experiment_name"]
     writer = SummaryWriter(log_dir="log_{}".format(experiment_name))
-    lr = opt.initial_lr
+    lr = opt["initial_lr"]
     for epoch in tqdm(range(1, num_epochs + 1), desc="Epochs"):
         batch_cnt = 0
         lr = scheduler(epoch, lr)
@@ -84,7 +68,7 @@ def tl(opt):
         for i, train_batch in enumerate(train_dataloader):
             train_image, train_mask = train_batch['image'], train_batch['mask']
             train_image = train_image.cuda()
-            if opt.problem == 'wgc':
+            if problem == 'wgc':
                 one_hot_mask = torch.nn.functional.one_hot(train_mask.long(), num_classes=4).squeeze(-1)
                 train_mask = one_hot_mask.cuda().float()
                 prediction = model(train_image)
@@ -119,8 +103,3 @@ def tl(opt):
         end_time = time.time()
         total_time = end_time - start_time
         tqdm.write("Epoch {} took {:.2f} seconds.".format(epoch, total_time))
-
-
-if __name__ == '__main__':
-    options = create_parser()
-    tl(options)
